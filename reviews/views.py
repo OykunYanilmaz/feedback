26f0381b61087cc3764a130351cd90469bee5ab4
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-10 17:16:02 +0300

    Using class based views

--- reviews/views.py
+++ reviews/views.py
@@ -1,46 +1,65 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 from .forms import ReviewForm
 from .models import Review
-
-# Create your views here.
-
-
-def review(request):
-    # if request.method == "POST":
-    #     entered_username = request.POST['username']
-
-    #     if entered_username == "" or len(entered_username) >= 100:
-    #         return render(request, "reviews/review.html", {
-    #             "has_error": True
-    #         })
-
-    #     print(entered_username)
-    #     return HttpResponseRedirect("/thank-you")
-
-    # return render(request, "reviews/review.html", {
-    #     "has_error": False
-    # })
-
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            # review = Review(user_name=form.cleaned_data["user_name"], 
-            #                 review_text=form.cleaned_data["review_text"], 
-            #                 rating=form.cleaned_data["rating"])
-            # review.save()
-
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-
-    else:
-        form = ReviewForm()
-
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
 
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
+
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+
+# def review(request):
+#     # if request.method == "POST":
+#     #     entered_username = request.POST['username']
+
+#     #     if entered_username == "" or len(entered_username) >= 100:
+#     #         return render(request, "reviews/review.html", {
+#     #             "has_error": True
+#     #         })
+
+#     #     print(entered_username)
+#     #     return HttpResponseRedirect("/thank-you")
+
+#     # return render(request, "reviews/review.html", {
+#     #     "has_error": False
+#     # })
+
+#     if request.method == "POST":
+#         form = ReviewForm(request.POST)
+
+#         if form.is_valid():
+#             # review = Review(user_name=form.cleaned_data["user_name"],
+#             #                 review_text=form.cleaned_data["review_text"],
+#             #                 rating=form.cleaned_data["rating"])
+#             # review.save()
+
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
+
+#     else:
+#         form = ReviewForm()
+
+#     return render(request, "reviews/review.html", {
+#         "form": form
+#     })
