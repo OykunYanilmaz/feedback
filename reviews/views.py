fa0750a17ce2e80903c2484dadffd8855543dee8
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-10 16:38:10 +0300

    Validations and customizations of the form and  addind db

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
@@ -25,10 +26,14 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            review = Review(user_name=form.cleaned_data["user_name"], 
+                            review_text=form.cleaned_data["review_text"], 
+                            rating=form.cleaned_data["rating"])
+            review.save()
             return HttpResponseRedirect("/thank-you")
 
-    form = ReviewForm()
+    else:
+        form = ReviewForm()
 
     return render(request, "reviews/review.html", {
         "form": form
