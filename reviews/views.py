0d6b7b842adb7eca1d7d52e4c470f59d78802ab4
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-10 19:20:25 +0300

    Create View

--- reviews/views.py
+++ reviews/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 from .forms import ReviewForm
 from .models import Review
 
@@ -42,34 +42,41 @@ from .models import Review
 #         "form": form
 #     })
 
-
-class ReviewView(FormView):
+class ReviewView(CreateView):
+    model = Review
+    # fields = "__all__"
     form_class = ReviewForm
     template_name = "reviews/review.html"
-
-    # def get(self, request):
-    #     form = ReviewForm()
-
-    #     return render(request, "reviews/review.html", {
-    #         "form": form
-    #     })
-
     success_url = "/thank-you"
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+
+
+# class ReviewView(FormView):
+#     form_class = ReviewForm
+#     template_name = "reviews/review.html"
+
+#     # def get(self, request):
+#     #     form = ReviewForm()
+
+#     #     return render(request, "reviews/review.html", {
+#     #         "form": form
+#     #     })
+
+#     success_url = "/thank-you"
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
     
 
-    # def post(self, request):
-    #     form = ReviewForm(request.POST)
+#     # def post(self, request):
+#     #     form = ReviewForm(request.POST)
 
-    #     if form.is_valid():
-    #         form.save()
-    #         return HttpResponseRedirect("/thank-you")
+#     #     if form.is_valid():
+#     #         form.save()
+#     #         return HttpResponseRedirect("/thank-you")
 
-    #     return render(request, "reviews/review.html", {
-    #         "form": form
-    #     })
+#     #     return render(request, "reviews/review.html", {
+#     #         "form": form
+#     #     })
 
 
 # def thank_you(request):
