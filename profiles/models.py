ed42dc2250902d4cbb3948da62dc2c8640099daa
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-02-11 20:07:43 +0300

    Using an imagefield and a createview

--- profiles/models.py
+++ profiles/models.py
@@ -3,4 +3,6 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="images")
+    # image = models.FileField(upload_to="images")
+    image = models.ImageField(upload_to="images")
+
